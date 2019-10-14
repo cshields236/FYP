85ad3421df8540eff3de05b4cab25946c767aaa5
Conor Shields <cshields236@gmail.com>
2019-10-14 22:05:54 +0100

    draw rectangle method

--- app/src/main/java/com/example/fyp/Helper/RectOverlay.java
+++ app/src/main/java/com/example/fyp/Helper/RectOverlay.java
@@ -5,6 +5,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.graphics.RectF;
 import android.util.AttributeSet;
 
 public class RectOverlay extends GraphicOverlay.Graphic {
@@ -32,6 +33,12 @@ public class RectOverlay extends GraphicOverlay.Graphic {
 
     @Override
     public void draw(Canvas canvas) {
+        RectF rectF = new RectF(rect);
+        rectF.left = translateX(rectF.left);
+        rectF.right = translateX(rectF.right);
+        rectF.top = translateY(rectF.top);
+        rectF.bottom = translateY(rectF.bottom);
 
+        canvas.drawRect(rectF, rectPaint);
     }
 }
