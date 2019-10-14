de7539db5d4b61b2f11d70217adb6f63f155ed1e
Conor Shields <cshields236@gmail.com>
2019-10-14 22:20:11 +0100

    layout with overlay

--- app/src/main/java/com/example/fyp/MainActivity.java
+++ app/src/main/java/com/example/fyp/MainActivity.java
@@ -6,16 +6,22 @@ import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
 
+import com.example.fyp.Helper.GraphicOverlay;
 import com.google.firebase.ml.vision.common.FirebaseVisionImageMetadata;
 import com.google.firebase.ml.vision.face.FirebaseVisionFace;
+import com.wonderkiln.camerakit.CameraView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 
 
     }
