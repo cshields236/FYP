d3ebb4f7ee8d3d8c0e671b8e323761b90c3f301e
Conor Shields <cshields236@gmail.com>
2019-10-14 23:16:57 +0100

    detect face works with photos

--- app/src/main/java/com/example/fyp/MainActivity.java
+++ app/src/main/java/com/example/fyp/MainActivity.java
@@ -1,28 +1,158 @@
 package com.example.fyp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.graphics.Bitmap;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.fyp.Helper.GraphicOverlay;
+import com.example.fyp.Helper.RectOverlay;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
 import com.google.firebase.ml.vision.common.FirebaseVisionImageMetadata;
 import com.google.firebase.ml.vision.face.FirebaseVisionFace;
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetector;
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetectorOptions;
+import com.wonderkiln.camerakit.CameraKit;
+import com.wonderkiln.camerakit.CameraKitError;
+import com.wonderkiln.camerakit.CameraKitEvent;
+import com.wonderkiln.camerakit.CameraKitEventListener;
+import com.wonderkiln.camerakit.CameraKitImage;
+import com.wonderkiln.camerakit.CameraKitVideo;
 import com.wonderkiln.camerakit.CameraView;
+
+import java.util.List;
+
+import dmax.dialog.SpotsDialog;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    CameraView cameraView;
+    GraphicOverlay graphicOverlay;
+    Button btnDetect;
+
+
+    AlertDialog waitingDialog ;
+    @Override
+    protected void onResume() {
+        super.onResume();
+        cameraView.setFacing(CameraKit.Constants.FACING_FRONT);
+        cameraView.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        cameraView.setFacing(CameraKit.Constants.FACING_FRONT);
+        cameraView.stop();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //initializing view
+        cameraView = (CameraView) findViewById(R.id.camera_view);
+        graphicOverlay = (GraphicOverlay) findViewById(R.id.graphic_overlay);
+        btnDetect = (Button) findViewById(R.id.btn_detect);
+
+        cameraView.setFacing(CameraKit.Constants.FACING_FRONT);
+
+        waitingDialog = new SpotsDialog.Builder().setContext(this)
+                .setMessage("Please wait")
+                .setCancelable(false)
+                .build();
+
+        btnDetect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cameraView.start();
+                cameraView.captureImage();
+                graphicOverlay.clear();
+            }
+        });
+        cameraView.addCameraKitListener(new CameraKitEventListener() {
+            @Override
+            public void onEvent(CameraKitEvent cameraKitEvent) {
+
+            }
+
+            @Override
+            public void onError(CameraKitError cameraKitError) {
+
+            }
+
+            @Override
+                public void onImage(CameraKitImage cameraKitImage) {
+                    waitingDialog.show();
+
+                    Bitmap bitmap = cameraKitImage.getBitmap();
+                    bitmap = Bitmap.createScaledBitmap(bitmap,cameraView.getWidth(), cameraView.getHeight(), false);
+                    cameraView.stop();
+
+                    runFaceDetector(bitmap);
+
+                }
+
+            @Override
+            public void onVideo(CameraKitVideo cameraKitVideo) {
+
+            }
+        });
 
 
 
+    }
+
+    private void runFaceDetector(Bitmap bitmap) {
+
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(bitmap);
+
+        FirebaseVisionFaceDetectorOptions options = new FirebaseVisionFaceDetectorOptions.Builder().build();
+
+        FirebaseVisionFaceDetector detector = FirebaseVision.getInstance().getVisionFaceDetector(options);
+
+        detector.detectInImage(image).addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionFace>>() {
+            @Override
+            public void onSuccess(List<FirebaseVisionFace> firebaseVisionFaces) {
+                processFaceResult(firebaseVisionFaces);
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void processFaceResult(List<FirebaseVisionFace> firebaseVisionFaces) {
+
+        int count = 0;
+        for(FirebaseVisionFace face : firebaseVisionFaces)
+        {
+            Rect bounds = face.getBoundingBox();
+
+            RectOverlay rect = new RectOverlay(graphicOverlay, bounds);
+            graphicOverlay.add(rect);
+
+            count++;
+        }
+        waitingDialog.dismiss();
+        Toast.makeText(this, String.format("Detected %d faces", count++), Toast.LENGTH_LONG).show();
     }
 }
