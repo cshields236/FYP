c7b8373880b0a9454672abe44bf316eb8fb18adb
Conor Shields <42182485+cshields236@users.noreply.github.com>
2019-10-23 22:27:41 +0100

    sign in validation working

--- app/src/main/java/com/example/fyp/App/LogInActivity.java
+++ app/src/main/java/com/example/fyp/App/LogInActivity.java
@@ -3,12 +3,19 @@ package com.example.fyp.App;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.fyp.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -16,6 +23,7 @@ public class LogInActivity extends AppCompatActivity {
 
     FirebaseAuth mFirebaseAuth;
     public FirebaseAuth.AuthStateListener mAuthStateListener;
+    private FirebaseAuth mAuth;
 
 
     @Override
@@ -24,24 +32,40 @@ public class LogInActivity extends AppCompatActivity {
         setContentView(R.layout.activity_log_in);
 
 
+
+
+
+    }
+
+    public void signIn(View view){
+        mAuth = FirebaseAuth.getInstance();
+
+
         mFirebaseAuth = FirebaseAuth.getInstance();
         final EditText emailtxt = findViewById(R.id.lEmail);
         final EditText passwordtxt = findViewById(R.id.lPassword);
-        Button registerbtn = findViewById(R.id.lButton);
-        TextView tvSignIn = findViewById(R.id.lLink);
 
-//        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
-//            FirebaseUser firebaseUser;
-//
-//            {
-//                firebaseUser = mFirebaseAuth.getCur  rentUser();
-//            }
-//           // if(fire)
-//
-//            @Override
-//            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-//
-//            }
-//        //}
-//    }
-}}
+        String email = emailtxt.getText().toString();
+        String password = passwordtxt.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+
+                    Log.d("", "signInWithEmail:success");
+                    FirebaseUser user = mAuth.getCurrentUser();
+
+                    startActivity(new Intent(LogInActivity.this, MainActivity.class));
+                }
+                else{
+                    Log.w("", "signInWithEmail:failure", task.getException());
+                }
+                Toast.makeText(LogInActivity.this, "Authentication failed." + task.getException().getMessage(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+}
