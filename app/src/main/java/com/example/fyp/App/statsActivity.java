2d194d9984175e557a03179bb1ff001da4832f56
Conor Shields <42182485+cshields236@users.noreply.github.com>
2020-02-10 00:27:34 +0000

    getting info from DB and displaying in Log

--- app/src/main/java/com/example/fyp/App/statsActivity.java
+++ app/src/main/java/com/example/fyp/App/statsActivity.java
@@ -10,14 +10,19 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.fyp.R;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class statsActivity extends AppCompatActivity {
     Button btn;
@@ -39,21 +44,35 @@ public class statsActivity extends AppCompatActivity {
              public void onClick(View v) {
                  FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
                  FirebaseFirestore db = FirebaseFirestore.getInstance();
-                 DocumentReference ref = db.collection("users").document(user.getUid()).collection("Journeys").document();
+                 CollectionReference ref = db.collection("users").document(user.getUid()).collection("Journeys");
 
 
-                 // Writing a face analysis to the database once every second
-                 ref.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                 ref.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                      @Override
-                     public void onSuccess(DocumentSnapshot documentSnapshot) {
-                       txt.setText( documentSnapshot.toString());
-                     }
-                 }).addOnFailureListener(new OnFailureListener() {
-                     @Override
-                     public void onFailure(@NonNull Exception e) {
+                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                         if (task.isSuccessful()) {
+                             for (QueryDocumentSnapshot document : task.getResult()) {
+                                 Log.d(TAG, "onComplete: " + document.getId() + " => " + document.getData());
+                             }
+                         } else {
+                             Log.d(TAG, "Error getting documents: ", task.getException());
+                         }
 
                      }
                  });
+
+                 // Writing a face analysis to the database once every second
+//                 ref.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+//                     @Override
+//                     public void onSuccess(DocumentSnapshot documentSnapshot) {
+//                       txt.setText( documentSnapshot.get);
+//                     }
+//                 }).addOnFailureListener(new OnFailureListener() {
+//                     @Override
+//                     public void onFailure(@NonNull Exception e) {
+//
+//                     }
+//                 });
              }
          });
     }
